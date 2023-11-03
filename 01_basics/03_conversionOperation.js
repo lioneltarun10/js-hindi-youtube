1bf4a0a864a647a8c03fc142870e156281fb6226
lioneltarun10 <60603445+lioneltarun10@users.noreply.github.com>
2023-11-03 12:25:35 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "tarun"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// conversion to number
+//"33" => 33
+//"33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = NaN
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+//conversion to Boolean
+//1 => true, 0 => false
+// "" => false
+// "tarun" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+ 
