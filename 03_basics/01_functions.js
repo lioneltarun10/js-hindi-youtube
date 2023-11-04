330371ff6d5c8c6db79c3e075e1fd21d3e20ee15
lioneltarun10 <60603445+lioneltarun10@users.noreply.github.com>
2023-11-04 12:22:33 +0000

    Rest Operator in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,35 @@ function loginUserMessage(username= "sam"){
 }
 
 // console.log(loginUserMessage("tarun"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+//rest operator
+function calculateCartPrice(val1,val2,...num1){
+   return num1
+}
+
+console.log(calculateCartPrice(2,400,500,800,200))
+
+const user ={
+    username: "tarun",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+   return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,1000]));
