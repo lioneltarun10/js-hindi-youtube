e7cd0c27b086fcd110a3cb64ca02d8daa290c997
lioneltarun10 <60603445+lioneltarun10@users.noreply.github.com>
2023-11-04 16:26:52 +0000

    nested scope , example of hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,18 +1,62 @@
 // var c = 300
 let a =300
+// if(true){
+//     let a =10
+//     const b = 20
+//     console.log("Inner: ",a);
+//     // c = 30
+// }
+
+// for(let i=0;i<Array.length;i++){
+//     const element = array[i]
+// }
+
+//nested scope
+
+function one(){
+    const username="tarun"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
 if(true){
-    let a =10
-    const b = 20
-    console.log("Inner: ",a);
-    // c = 30
+    const username="tarun"
+    if(username === "tarun"){
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
 }
 
-for(let i=0;i<Array.length;i++){
-    const element = array[i]
-}
+// console.log(username);
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//+++++++++ interesting+++++++++
+
+console.log(addone(5))
+
+function addone(num){
+   return num+1
+} 
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num+2
+}
+
