09385ecea4e9058caba3914037e8ed701bae0425
lioneltarun10 <60603445+lioneltarun10@users.noreply.github.com>
2023-11-04 10:50:16 +0000

    Singleton Object , optional chaining, Object.assign , extracting only keys, values , entries and hasOwnProperty method

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,68 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "tarun"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "tarun",
+            lastname: "unnam"
+        }
+    }
+}
+
+//optional chaining
+console.log(regularUser.fullname?.userfullname.firstname);
+
+const obj1 ={
+    1:'a',2:'b'
+}
+const obj2 ={
+    3:'a',4:'b'
+}
+const obj4 ={
+    5:'a',6:'b'
+}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2)
+// there's difference between both
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 = {
+    ...obj1,...obj2,...obj4
+}
+
+// console.log(obj3);
+// console.log(obj1);
+
+const users = [
+    {
+        id:1,
+        email:'tarun@gmail.com'
+    },
+    {
+        id:1 ,
+        email:'tarun@gmail.com'
+    },
+    {
+        id:1 ,
+        email:'tarun@gmail.com'
+    }
+]
+
+// console.log(users[1].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
