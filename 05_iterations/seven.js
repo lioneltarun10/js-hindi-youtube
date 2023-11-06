4fe057be426664041431952d0fdcb4b75b78c05a
lioneltarun10 <60603445+lioneltarun10@users.noreply.github.com>
2023-11-06 11:09:02 +0000

    Map , chaining of filter , map , reduce

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+// Map higher order function
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumbers.map((num)=>num+10)
+
+//chaining of map ,filter
+
+const newNums = myNumbers
+                .map((num)=>num*10)
+                .map((num)=> num+1)
+                .filter((num)=> num>=40)
+
+
+console.log(newNums);
